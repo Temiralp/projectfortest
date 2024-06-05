37ab3222188ccc931478497de4186944f0cd969b
sinan <sinansallioglu@gmail.com>
2024-06-05 22:24:39 +0300

    main değiştirildi

--- loginuser.cs
+++ loginuser.cs
@@ -1,13 +1,11 @@
 public class LoginUser{
 Console.WriteLine("test");
-sinan1
-test
+deðiþtirildi
+main deðiþtirildi
 
-<<<<<<< HEAD
 
 test
 test sinan1
 
-=======
->>>>>>> 3832771659718aba9e2fc9a6a804fabac7dc642d
+
 }
